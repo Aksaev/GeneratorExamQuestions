f622985d0a03f1a7367bd1efb99b2f3e9ec11798
Aksaev <mail-ax@ya.ru>
2023-05-29 21:46:52 +0400

    Initial commit

---
+++ src/test/java/me/aksaev/GeneratorExamQuestions/GeneratorExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.GeneratorExamQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorExamQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
