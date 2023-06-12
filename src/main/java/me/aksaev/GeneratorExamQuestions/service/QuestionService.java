0dc27ebe91d5f8d28ae9b48100a7d120755645fa
Aksaev <mail-ax@ya.ru>
2023-06-12 22:04:32 +0400

    Курсовая работа Java Core. Выполнил.

---
+++ src/main/java/me/aksaev/GeneratorExamQuestions/service/QuestionService.java
@@ -0,0 +1,13 @@
+package me.aksaev.GeneratorExamQuestions.service;
+
+import me.aksaev.GeneratorExamQuestions.model.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+    Question add(String question, String answer);
+    Question add(Question question);
+    Question remove(Question question);
+    Collection<Question> getAll();
+    Question getRandomQuestion();
+}
